c8ee28b181a496377378cab7b2d57d1ab97926c9
Eliseo Flores <eliseo_flores530@yahoo.com>
2021-05-07 20:00:18 -0700

    changed db address manually

--- starter/models.py
+++ starter/models.py
@@ -12,7 +12,7 @@ setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://otunigjyxkpepb:ca4d081c5f0181e5a24c50a9fb5ab2a27a69376a5aecb678f2c88f9229baaff3@ec2-34-200-94-86.compute-1.amazonaws.com:5432/d6nh77t5l8835'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
